fafd6a144d366e36df7034fe656d16f71b674a51
michalw <michalwlocka@gmail.com>
2022-07-18 10:01:33 +0200

    Initial commit

---
+++ src/main/java/com/javalbd/spring2lbd/Spring2LbdApplication.java
@@ -0,0 +1,13 @@
+package com.javalbd.spring2lbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2LbdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring2LbdApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/javalbd/spring2lbd/entity/Student.java
@@ -0,0 +1,2 @@
+package com.javalbd.spring2lbd.entity;public class Student {
+}
