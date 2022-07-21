292cf75767b6cef6058ee376b36cb2a4e5ba2a70
Michal <michalwlocka@gmail.com>
2022-07-21 23:59:24 +0200

    working JWT authentication (corrected README)

--- src/main/java/com/javalbd/spring2lbd/filter/jsonauth/AuthenticationSuccessHandler.java
+++ src/main/java/com/javalbd/spring2lbd/filter/jsonauth/AuthenticationSuccessHandler.java
@@ -1,9 +1,8 @@
-package com.javalbd.spring2lbd.jwt;
+package com.javalbd.spring2lbd.filter.jsonauth;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
